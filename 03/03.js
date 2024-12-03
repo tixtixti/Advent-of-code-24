a04a2b2f90b7e7faf9872a8ed85088149cf9b188
Timo Tuononen <timo.tuononen@hogrefe.fi>
2024-12-03 09:22:21 +0200

    pretty

--- 03/03.js
+++ 03/03.js
@@ -6,11 +6,10 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     return;
   }
 
-  const a = data.match(/mul\(\d{1,3},\d{1,3}\)/g).map((node) => {
-    return node.match(/\d{1,3},\d{1,3}/g)[0].split(",");
-  });
-  const abba = a.reduce((prev, curr) => {
-    return prev + curr[0] * curr[1];
-  }, 0);
-  console.log(abba);
+  console.log(
+    data
+      .match(/mul\(\d{1,3},\d{1,3}\)/g)
+      .map((node) => node.match(/\d{1,3},\d{1,3}/g)[0].split(","))
+      .reduce((prev, curr) => prev + curr[0] * curr[1], 0)
+  );
 });
