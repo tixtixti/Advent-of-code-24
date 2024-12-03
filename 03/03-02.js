7fdeabaf8f8c576f3eea09f576db2c0da478c09d
Timo Tuononen <timo.tuononen@hogrefe.fi>
2024-12-03 09:17:57 +0200

    03-02

---
+++ 03/03-02.js
@@ -0,0 +1,23 @@
+const fs = require("node:fs");
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const a = data.match(/(don't\(\))|(do\(\))|(mul\(\d{1,3},\d{1,3}\))/g);
+
+  let isCaluclating = true;
+
+  const babba = a.reduce((score, node) => {
+    if (node.match(/(do\(\))/g)) isCaluclating = true;
+    if (node.match(/(don't\(\))/g)) isCaluclating = false;
+    if (isCaluclating && node.match(/\d{1,3},\d{1,3}/g)) {
+      const abba = node.match(/\d{1,3},\d{1,3}/g)[0].split(",");
+      return score + abba[0] * abba[1];
+    }
+    return score;
+  }, 0);
+  console.log(babba);
+});
