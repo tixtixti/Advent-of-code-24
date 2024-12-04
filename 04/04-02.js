25f0ca512bd1839fb9a90b99c4557d2c581c5bba
tixtixti <timo@achievely.co>
2024-12-04 18:22:20 +0200

    add prettier conf + refactor

--- 04/04-02.js
+++ 04/04-02.js
@@ -1,41 +1,41 @@
-const fs = require("node:fs");
+const fs = require('node:fs')
 
-fs.readFile("input.txt", "utf8", (err, data) => {
+fs.readFile('input.txt', 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
-    return;
+    console.error(err)
+    return
   }
   // make 2d matrix
-  const matrix2d = data.split("\n").map((node) => node.split(""));
+  const matrix2d = data.split('\n').map((node) => node.split(''))
 
-  let count = 0;
+  let count = 0
   matrix2d.forEach((row, rowIndex) => {
     row.forEach((node, idex) => {
-      const currentMarker = node;
+      const currentMarker = node
       if (isEitherSorM(node)) {
         // correct start
         if (
           matrix2d[rowIndex + 1] &&
-          matrix2d[rowIndex + 1][idex + 1] === "A"
+          matrix2d[rowIndex + 1][idex + 1] === 'A'
         ) {
           if (matrix2d[rowIndex + 2]) {
-            lastMarker = matrix2d[rowIndex + 2][idex + 2];
+            lastMarker = matrix2d[rowIndex + 2][idex + 2]
             if (isOtherThanPrevious(lastMarker, currentMarker)) {
-              const backMarker = matrix2d[rowIndex + 2][idex];
+              const backMarker = matrix2d[rowIndex + 2][idex]
               if (isEitherSorM(backMarker, lastMarker)) {
                 if (isOtherThanPrevious(row[idex + 2], backMarker)) {
-                  count++;
+                  count++
                 }
               }
             }
           }
         }
       }
-    });
-  });
-  console.log(count);
-});
+    })
+  })
+  console.log(count)
+})
 
-const isEitherSorM = (input) => input === "S" || input == "M";
+const isEitherSorM = (input) => input === 'S' || input == 'M'
 const isOtherThanPrevious = (input, prvevious) =>
-  (input === "S" && prvevious === "M") || (input == "M" && prvevious === "S");
+  (input === 'S' && prvevious === 'M') || (input == 'M' && prvevious === 'S')
--- 04/04.js
+++ 04/04.js
@@ -1,114 +1,107 @@
-const fs = require("node:fs");
+const fs = require('node:fs')
 
-fs.readFile("input.txt", "utf8", (err, data) => {
+fs.readFile('input.txt', 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
-    return;
+    console.error(err)
+    return
   }
   // make 2d matrix
-  const matrix2d = data.split("\n").map((node) => node.split(""));
+  const matrix2d = data.split('\n').map((node) => node.split(''))
   //  console.log(matrix2d);
 
-  let amountOfXmas =
+  console.log(
     calculateRows(matrix2d) +
-    calcucateColumns(matrix2d) +
-    calculateDiagonals(matrix2d);
+      calcucateColumns(matrix2d) +
+      calculateDiagonals(matrix2d)
+  )
 
   // calculate diagonals
-
-  console.log({ amountOfXmas });
-
-  console.log(calculateDiagonals(matrix2d));
-});
+})
 
 const countXmas = (sampleArray) => {
-  const xmasMatcher = /XMAS/g;
-  const samxMatcher = /XSAM/g;
-  const stringedArray = sampleArray.join("");
-  const xmmatches = stringedArray.match(xmasMatcher);
-  const smmatches = stringedArray.match(samxMatcher);
-  const abba = xmmatches ? xmmatches.length : 0;
-  const babba = smmatches ? smmatches.length : 0;
-  return abba + babba;
-};
+  const xmasMatcher = /XMAS/g
+  const samxMatcher = /SAMX/g
+  const stringedArray = sampleArray.join('')
+  const xmmatches = stringedArray.match(xmasMatcher)
+  const smmatches = stringedArray.match(samxMatcher)
+  const abba = xmmatches ? xmmatches.length : 0
+  const babba = smmatches ? smmatches.length : 0
+  return abba + babba
+}
 
-const calculateRows = (matrix) => {
-  return matrix.reduce((prev, row) => {
-    // const backToBack = row.join("");
-    // console.log(backToBack.match(xmasMatcher));
-    return prev + countXmas(row);
-  }, 0);
-};
+const calculateRows = (matrix) =>
+  matrix.reduce((prev, row) => prev + countXmas(row), 0)
 
 const calcucateColumns = (matrix) => {
   return matrix.reduce((prev, row, index, self) => {
-    let pystyrivi = [];
+    let pystyrivi = []
     row.forEach((item, indexCol) => {
-      pystyrivi.push(self[indexCol][index]);
-    });
-    // console.log(pystyrivi.join(""));
-    return prev + countXmas(pystyrivi);
-  }, 0);
-};
+      try {
+        pystyrivi.push(self[indexCol][index])
+      } catch (e) {}
+    })
+    return prev + countXmas(pystyrivi)
+  }, 0)
+}
 
 const calculateDiagonals = (matrix) => {
-  let allDiagonals = [];
-  const numRows = matrix.length; // montako riviä
-  const numCols = matrix[0].length; // montako per rivi
+  let allDiagonals = []
+  const numRows = matrix.length // montako riviä
+  const numCols = matrix[0].length // montako per rivi
 
   for (let start = 0; start < numRows; start++) {
-    let diagonal = [];
+    let diagonal = []
     let row = start,
-      col = 0;
+      col = 0
     while (row < numRows && col < numCols) {
-      diagonal.push(matrix[row][col]);
-      row++;
-      col++;
+      diagonal.push(matrix[row][col])
+      row++
+      col++
     }
-    allDiagonals.push(diagonal);
+    allDiagonals.push(diagonal)
     // console.log({ diagonal, count: countXmas(diagonal) });
   }
 
   for (let start = 1; start < numCols; start++) {
-    let diagonal = [];
+    let diagonal = []
     let row = 0,
-      col = start;
+      col = start
     while (row < numRows && col < numCols) {
-      diagonal.push(matrix[row][col]);
-      row++;
-      col++;
+      diagonal.push(matrix[row][col])
+      row++
+      col++
     }
-    allDiagonals.push(diagonal);
+    allDiagonals.push(diagonal)
     //   console.log({ diagonal, count: countXmas(diagonal) });
   }
 
   for (let start = 0; start < numRows; start++) {
-    let diagonal = [];
+    let diagonal = []
     let row = start,
-      col = numCols - 1;
+      col = numCols - 1
     while (row < numRows && col >= 0) {
-      diagonal.push(matrix[row][col]);
-      row++;
-      col--;
+      diagonal.push(matrix[row][col])
+      row++
+      col--
     }
-    allDiagonals.push(diagonal);
+    allDiagonals.push(diagonal)
     // console.log({ diagonal, count: countXmas(diagonal) });
   }
 
   for (let start = numCols - 2; start >= 0; start--) {
-    let diagonal = [];
+    let diagonal = []
     let row = 0,
-      col = start;
+      col = start
     while (row < numRows && col >= 0) {
-      diagonal.push(matrix[row][col]);
-      row++;
-      col--;
+      diagonal.push(matrix[row][col])
+      row++
+      col--
     }
-    allDiagonals.push(diagonal);
+    allDiagonals.push(diagonal)
     //  console.log({ diagonal, count: countXmas(diagonal) });
   }
 
   return allDiagonals.reduce((prev, curr) => {
-    return prev + countXmas(curr);
-  }, 0);
-};
+    return prev + countXmas(curr)
+  }, 0)
+}
