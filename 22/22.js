4dde6c16873e2fa0753a38b9fcab37840bdcc1ff
tixtixti <timo@achievely.co>
2024-12-22 19:09:34 +0200

    22-02

--- 22/22.js
+++ 22/22.js
@@ -1,23 +1,49 @@
-const results = {
-    1: 8685429,
-    10: 4700978,
-    100: 15273692,
-    2024: 8667524,
-}
 const fs = require('node:fs').promises
 
 async function main() {
     const map = (await fs.readFile('./input.txt', 'utf8')).split('\n')
+    let resultsMap = new Map()
     const res = map.reduce((prev, key) => {
         let tempRes = key
-
+        let tempDigit = null
+        let prevTempDigit = key % 10
+        let last4Sequences = []
+        const roundSet = new Set()
         for (let index = 0; index < 2000; index++) {
             tempRes = Math.abs(stepThree(stepTwo(stepOne(tempRes))))
+            tempDigit = tempRes % 10 // last digit
+            let currentSequence = tempDigit - prevTempDigit // diff
+            last4Sequences.push(currentSequence) // save current to slot
+            prevTempDigit = tempDigit // save current last digit to for next
+            if (index > 3) {
+                last4Sequences.shift()
+                // once array is full
+                let roundKey = last4Sequences.toString() // make key of array
+                if (!roundSet.has(last4Sequences.toString())) {
+                    //check if _roundSet_ has value
+                    roundSet.add(roundKey) // add it there
+                    if (!resultsMap.get(roundKey)) {
+                        // while if _roundSet has _no_ value check if this is in results map
+                        resultsMap.set(roundKey, tempDigit) // set if no, with last digits as value
+                    } else {
+                        // is not in roundSet but is in resultMap
+                        resultsMap.set(
+                            roundKey,
+                            resultsMap.get(roundKey) + tempDigit
+                        ) // update value with lastDigit
+                    }
+                } else {
+                    // do nothing
+                }
+            }
         }
         return prev + tempRes
     }, 0)
-
-    console.log(res)
+    console.log(
+        resultsMap.values().reduce((prev, curr) => {
+            return Math.max(prev, curr)
+        }, 0)
+    )
 }
 
 const div = (x, y) => Math.floor(x / y)
