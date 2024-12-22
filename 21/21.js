ba3d4768ca71f3ddf4a3aedb36275951cb00c440
tixtixti <timo@achievely.co>
2024-12-22 22:01:52 +0200

    wip älä käytä

--- 21/21.js
+++ 21/21.js
@@ -15,39 +15,39 @@ const keyPad = [
 ]
 const abba = {
     '>': {
-        '^': ['<', '^'],
-        A: ['^'],
-        v: ['<'],
-        '<': ['<', '<'],
-        '>': [],
+        '^': ['<', '^', 'A'],
+        A: ['^', 'A'],
+        v: ['<', 'A'],
+        '<': ['<', '<', 'A'],
+        '>': ['A'],
     },
     v: {
-        '^': ['^'],
-        A: ['^', '>'],
-        '<': ['<'],
-        '>': ['>'],
-        v: [],
+        '^': ['^', 'A'],
+        A: ['^', '>', 'A'],
+        '<': ['<', 'A'],
+        '>': ['>', 'A'],
+        v: ['A'],
     },
     '^': {
-        A: ['>'],
-        v: ['v'],
-        '<': ['v', '<'],
-        '>': ['>', 'v'],
-        '^': [],
+        A: ['>', 'A'],
+        v: ['v', 'A'],
+        '<': ['v', '<', 'A'],
+        '>': ['>', 'v', 'A'],
+        '^': ['A'],
     },
     A: {
-        '^': ['<'],
-        v: ['v', '<'],
-        '<': ['v', '<', '<'],
-        '>': ['v'],
-        A: [],
+        '^': ['<', 'A'],
+        v: ['v', '<', 'A'],
+        '<': ['v', '<', '<', 'A'],
+        '>': ['v', 'A'],
+        A: ['A'],
     },
     '<': {
-        '^': ['>', '^'],
-        A: ['>', '>', '^'],
-        v: ['>'],
-        '>': ['>', '>'],
-        '<': [],
+        '^': ['>', '^', 'A'],
+        A: ['>', '>', '^', 'A'],
+        v: ['>', 'A'],
+        '>': ['>', '>', 'A'],
+        '<': ['A'],
     },
 }
 
@@ -72,8 +72,8 @@ function generateCombinations(lines) {
 }
 
 //const routes = ['029A', '980A', '179A', '456A', '379A'] // test
-const routes = ['540A', '839A', '682A', '826A', '974A'] //real
-//const routes = ['029A']
+//const routes = ['540A', '839A', '682A', '826A', '974A'] //real
+const routes = ['029A']
 //const routes = ['456A']
 
 // eka setti breah first ja sitten vaan käyt kaikki läpi?`
@@ -99,26 +99,70 @@ const secondarySteps = (route) => {
     //console.log(route)
     return route.split('').reduce((prev, curr, index, self) => {
         let startCoord = index === 0 ? A : self[index - 1]
-        return prev + abba[startCoord][curr].join('') + A
+        return prev + abba[startCoord][curr].join('')
     }, '')
 }
 
+const calculateSingleInputTimes = (char, prev, temp, i, precomputedResults) => {
+    if (i === 0) {
+        return temp
+    }
+    const key = `${char}_${i}`
+    if (precomputedResults.has(key)) {
+        return precomputedResults.get(key) // Return cached result
+    }
+
+    let allChanges = abba[prev][char]
+    let result = 0
+    allChanges.forEach((change) => {
+        result += calculateSingleInputTimes(
+            change,
+            char,
+            temp + allChanges.length,
+            i - 1,
+            precomputedResults
+        )
+    })
+    precomputedResults.set(key, result)
+    return result
+}
+const precomputedResults = new Map()
 const res = routes.reduce((prev, route) => {
     const allRoutesAsString = handleStep(route, numPad)
-    const midSteps = allRoutesAsString.map((node) => {
-        return secondarySteps(node)
+
+    let tempSteps = allRoutesAsString[2]
+    console.log(tempSteps)
+
+    let result = 0
+    const stack = []
+
+    // Initialize the stack
+    tempSteps.split('').forEach((node) => {
+        stack.push({ item: node, remainingBlinks: 2 })
     })
 
-    const finalSteps = midSteps.map((node) => {
-        return secondarySteps(node)
-    })
+    while (stack.length > 0) {
+        const process = stack.shift()
 
-    const finalScore = finalSteps.reduce(
+        // Compute result on-demand and cache it
+        const computed = calculateSingleInputTimes(
+            process.item,
+            A,
+            result,
+            process.remainingBlinks,
+            precomputedResults
+        )
+        result += computed
+    }
+
+    /*
+    const finalScore = tempSteps.reduce(
         (prev, node) => Math.min(prev, node.length),
         20000
     )
-
+*/
+    console.log({ result })
     const other = Number(route.split('').splice(0, 3).join(''))
-    return prev + finalScore * other
+    return prev + result * other
 }, 0)
 console.log(res)
