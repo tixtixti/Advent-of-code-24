d5254cf2f6b1044cf61763eb6390f7e2a031eb7d
Timo Tuononen <timo.tuononen@hogrefe.fi>
2024-12-25 13:37:40 +0200

    21 char depth

--- 21/21.js
+++ 21/21.js
@@ -70,6 +70,7 @@ function generateCombinations(lines) {
     helper([], 0)
     return results
 }
+
 const routes = ['029A', '980A', '179A', '456A', '379A'] // test //126384
 //const routes = ['540A', '839A', '682A', '826A', '974A'] //real //278568
 //const routes = ['029A']
@@ -94,35 +95,93 @@ const handleStep = (route, graph) => {
     )
 }
 
-const secondaryStepsRecGPT = (routes, rounds) => {
-    const buildPaths = (route) => {
-        return route.map((node) => {
-            let result = ''
-            for (let i = 0; i < node.length; i++) {
-                const startCoord = i === 0 ? A : node[i - 1]
-                const curr = node[i]
-                result += abba[startCoord][curr].join('')
-            }
-            return result
+const secondarySteps = (route) => {
+    return route.split('').reduce((prev, curr, index, self) => {
+        let startCoord = index === 0 ? A : self[index - 1]
+        return prev + abba[startCoord][curr].join('')
+    }, '')
+}
+const secondaryStepsCache = (route, round) => {
+    //console.log(route)
+
+    return route.split('').reduce((prev, curr, index, self) => {
+        let startCoord = index === 0 ? A : self[index - 1]
+        //console.log(curr)
+        // check if this start_curr_round exists, if not. fully calculate that pair thing out all the rounds  and then proceed for next characters
+        return prev + calculateStepsWithStack(startCoord, curr, round)
+    }, 0)
+}
+
+const calculateStepsWithStack = (startCoord, curr, remainingRounds) => {
+    const key = `${startCoord}_${curr}_${remainingRounds}`
+
+    // Check if the result is cached
+    if (memoizationMap.has(key)) {
+        return memoizationMap.get(key)
+    }
+
+    // Initialize stack and variables
+    const stack = [{ startCoord, curr, remainingRounds }]
+    let totalPaths = 0
+
+    // Stack-based iterative processing
+    while (stack.length > 0) {
+        const { startCoord, curr, remainingRounds } = stack.pop()
+
+        // Base case: no more rounds
+        if (remainingRounds - 1 === 0) {
+            totalPaths += abba[startCoord][curr].length
+            continue
+        }
+
+        // Add next possible steps to the stack
+        const directions = abba[startCoord][curr]
+        directions.forEach((nextDir, index, self) => {
+            let startCoord = index === 0 ? A : self[index - 1]
+
+            stack.push({
+                startCoord,
+                curr: nextDir,
+                remainingRounds: remainingRounds - 1,
+            })
         })
     }
 
-    let currentRoutes = routes
-
-    for (let i = 0; i < rounds; i++) {
-        currentRoutes = buildPaths(currentRoutes)
-    }
-
-    return currentRoutes
+    // Cache the result
+    memoizationMap.set(key, totalPaths)
+    return totalPaths
 }
 
-const res = routes.reduce((prev, route) => {
-    const finalScore = handleStep(route, numPad)
-        .map((node) => secondaryStepsRecGPT([node], 2))
-        .flat()
-        .reduce((prev, node) => Math.min(prev, node.length), Infinity)
+const memoizationMap = new Map() // Cache for results
 
+const res = routes.reduce((prev, route) => {
+    const allRoutesAsString = handleStep(route, numPad)
+
+    let tempSteps = allRoutesAsString
+    let i = 0
+    /*
+    while (i < 2) {
+        tempSteps = tempSteps.map((node) => {
+            return secondarySteps(node, 2)
+        })
+
+        i++
+    }
+    const finalScore = tempSteps.reduce(
+        (prev, node) => Math.min(prev, node.length),
+        Infinity
+    )
+*/
+    const temp = allRoutesAsString.map((route) => secondaryStepsCache(route, 2))
+    const finalScore = temp.reduce(
+        (prev, node) => Math.min(prev, node),
+        Infinity
+    )
+    console.log({ finalScore })
     const other = Number(route.split('').splice(0, 3).join(''))
     return prev + finalScore * other
 }, 0)
 console.log(res)
+
+//console.log(secondaryStepsCache('<A<^A', 16))
+// console.log(secondaryStepsCache('<A^A>^^AvvvA', 2))
