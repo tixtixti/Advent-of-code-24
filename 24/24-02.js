4afe8b5a3fcbb8784b17c7b8830c7f2bcac4417f
Timo Tuononen <timo.tuononen@hogrefe.fi>
2024-12-28 16:18:27 +0200

    it is over

--- 24/24-02.js
+++ 24/24-02.js
@@ -2,36 +2,142 @@ const fs = require('node:fs').promises
 
 async function main() {
     const input = (await fs.readFile('./input.txt', 'utf8')).split('\n')
+    let { gates, startsObj } = handleInput(input)
+    inputValuesForStartsObj(startsObj, 'x', 1)
+    inputValuesForStartsObj(startsObj, 'y', 1)
 
-    let res = 0
-    const expected = 40
-    const original = res
-    let swapIndex = 1
-    while (res !== expected) {
-        console.log(swapIndex)
-        res = handleRound(JSON.parse(JSON.stringify(input)), swapIndex)
-        swapIndex++
+    let i = 0
+    let carryCode = null
+    while (i < 43) {
+        let searchX = i < 10 ? `x0${i}` : `x${i}`
+        let searchY = i < 10 ? `y0${i}` : `y${i}`
+        let searchZ = i < 10 ? `z0${i}` : `z${i}`
+
+        console.log({ searchX, searchY, searchZ })
+        if (i === 0) {
+            // let initialXor = findGate(gates, searchX, searchY, 'XOR')
+            let initialAnd = findGateOutput(gates, searchX, searchY, 'AND')
+            carryCode = initialAnd
+        } else {
+            let intermediateXor = findGateOutput(gates, searchX, searchY, 'XOR')
+
+            let carryInXor = findGateOutput(
+                gates,
+                carryCode,
+                intermediateXor,
+                'XOR'
+            )
+            if (carryInXor !== searchZ) {
+                throw new Error(
+                    `INCORRECT OUTPUT ${carryInXor} shouldBe: ${searchZ}`
+                )
+            }
+            let stepCarryGeneration = findGateOutput(
+                gates,
+                searchX,
+                searchY,
+                'AND'
+            )
+            let stepCarryPropagation = findGateOutput(
+                gates,
+                carryCode,
+                intermediateXor,
+                'AND'
+            )
+            let carryOut = findGateOutput(
+                gates,
+                stepCarryGeneration,
+                stepCarryPropagation,
+                'OR'
+            )
+            carryCode = carryOut
+        }
+        i++
     }
 }
-main()
+// main()
+// qjb gvw z15 jgc drg z22 jbp z35
+// x08 XOR y08 -> gvw
+// x08 AND y08 -> qjb
+// y15 AND x15 -> jgc
+// fbv XOR rgt -> z15
+// hwm AND tdc -> drg
+// tdc XOR hwm -> z22
+const sortLastOnes = [
+    'qjb',
+    'gvw',
+    'z15',
+    'jgc',
+    'drg',
+    'z22',
+    'jbp',
+    'z35',
+].sort()
+console.log(sortLastOnes.toString())
+const findGateOutput = (gates, xGate, yGate, operator) => {
+    let searchCodes = [xGate, yGate]
+    let gateFound = gates.find((gate) => {
+        return (
+            searchCodes.includes(gate.xGate) &&
+            searchCodes.includes(gate.yGate) &&
+            gate.operator == operator
+        )
+    })
+    if (!gateFound) {
+        let effected = gates.filter((gate) => {
+            return (
+                searchCodes.includes(gate.xGate) ||
+                searchCodes.includes(gate.yGate) ||
+                searchCodes.includes(gate.output)
+            )
+        })
+        console.log({ effected })
+        throw new Error(
+            `NOT FOUND x: ${xGate} y: ${yGate},operator: ${operator}`
+        )
+    }
+    return gateFound.output
+}
+const handleRound = (gates, startsObj) => {
+    let roundMap = new Map(startsObj)
+    let copyOfOriginalGates = JSON.parse(JSON.stringify(gates))
+    while (copyOfOriginalGates.length > 0) {
+        let notVisited = true
+        copyOfOriginalGates.forEach((node) => {
+            const { xGate, operator, yGate, output } = node
+            if (
+                roundMap.get(xGate) !== undefined &&
+                roundMap.get(yGate) !== undefined
+            ) {
+                notVisited = false
+                const xGateValue = roundMap.get(xGate)
+                const yGateValue = roundMap.get(yGate)
+                const operationResult = handleOperation(
+                    xGateValue,
+                    yGateValue,
+                    operator
+                )
+                node.calculated = true
+                roundMap.set(output, operationResult)
+            }
+        })
+        if (notVisited) {
+            return null
+        }
+        copyOfOriginalGates = copyOfOriginalGates.filter(
+            (node) => node.calculated === false
+        )
+
+        // console.log(gates.length)
+    }
+    const res = findBinaryForSymbol(roundMap, 'z')
+    return res
+}
 
 const andGate = (x, y) => x == 1 && y == 1
 const orGate = (x, y) => x == 1 || y == 1
 const xorGate = (x, y) => x != y
 
-const findIntergerForSymbol = (startsObj, symbol) => {
-    const Zkeys = Array.from(startsObj.keys()).filter(
-        (key) => key[0] === symbol
-    )
-    const resultArray = Array(Zkeys.length)
-    Zkeys.forEach((zkey) => {
-        const value = startsObj.get(zkey)
-        const [_, index] = zkey.split(symbol)
-        resultArray[Number(index)] = value
-    })
-    resultArray.reverse()
-    return parseInt(resultArray.join(''), 2)
-}
 const gateToBit = (x, y, fn) => (fn(x, y) ? 1 : 0)
 const handleOperation = (x, y, operation) => {
     switch (operation) {
@@ -61,44 +167,37 @@ const handleInput = (input) => {
     })
     return { gates, startsObj }
 }
+const findIntergerForSymbol = (startsObj, symbol) => {
+    const Zkeys = Array.from(startsObj.keys()).filter(
+        (key) => key[0] === symbol
+    )
+    const resultArray = Array(Zkeys.length)
+    Zkeys.forEach((zkey) => {
+        const value = startsObj.get(zkey)
+        const [_, index] = zkey.split(symbol)
+        resultArray[Number(index)] = value
+    })
+    resultArray.reverse()
+    return parseInt(resultArray.join(''), 2)
+}
 
-const handleRound = (input, swap) => {
-    let { gates, startsObj } = handleInput(input)
-    let temp = gates[0].output
-    let temp2 = gates[swap].output
-    gates[swap].output = temp
-    gates[0].output = temp2
-    // const inputX = findIntergerForSymbol(startsObj, 'x')
-    // const inputY = findIntergerForSymbol(startsObj, 'y')
-    //  console.log({ inputX, inputY })
-    let operationCompleted = false
-    let gatesToCalculate = JSON.parse(JSON.stringify(gates))
-    while (gatesToCalculate.length > 0) {
-        gatesToCalculate.forEach((node) => {
-            const { xGate, operator, yGate, output } = node
-            if (
-                startsObj.get(xGate) !== undefined &&
-                startsObj.get(yGate) !== undefined
-            ) {
-                operationCompleted = true
-                const xGateValue = startsObj.get(xGate)
-                const yGateValue = startsObj.get(yGate)
-                const operationResult = handleOperation(
-                    xGateValue,
-                    yGateValue,
-                    operator
-                )
-                node.calculated = true
-                startsObj.set(output, operationResult)
-            }
-        })
-        if (!operationCompleted) {
-            break
+const findBinaryForSymbol = (startsObj, symbol) => {
+    const Zkeys = Array.from(startsObj.keys()).filter(
+        (key) => key[0] === symbol
+    )
+    const resultArray = Array(Zkeys.length)
+    Zkeys.forEach((zkey) => {
+        const value = startsObj.get(zkey)
+        const [_, index] = zkey.split(symbol)
+        resultArray[Number(index)] = value
+    })
+    resultArray.reverse()
+    return resultArray.join('')
+}
+const inputValuesForStartsObj = (startsObj, symbolToChange, valueToInput) => {
+    Array.from(startsObj.keys()).map((key) => {
+        if (key[0] === symbolToChange) {
+            startsObj.set(key, valueToInput)
         }
-        gatesToCalculate = gatesToCalculate.filter(
-            (node) => node.calculated === false
-        )
-        // console.log(gates.length)
-    }
-    return findIntergerForSymbol(startsObj, 'z') // with test should be 40
+    })
 }
